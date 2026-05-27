0aaf4011ca4b72bd06c4a55c77d65864885f2889
marton-kovacs-0 <98marci@gmail.com>
2026-05-27 19:57:42 +0200

    Added additional tile textures, implemented drawTile helper method, rendered multiple tiles

--- game/core/src/main/java/com/marton/roguelike/Main.java
+++ game/core/src/main/java/com/marton/roguelike/Main.java
@@ -1,7 +1,6 @@
 package com.marton.roguelike;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -10,27 +9,50 @@ import com.marton.roguelike.render.TileAtlas;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private TextureRegion floorTile;
     private TileAtlas atlas;
+    private static final int RENDER_TILE_SIZE = 64;
+    private static final int MAP_OFFSET_X = 100;
+    private static final int MAP_OFFSET_Y = 100;
 
     @Override
     public void create() {
         atlas = new TileAtlas();
-        floorTile = atlas.getFloorTile();
         batch = new SpriteBatch();
-
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(floorTile, 140, 210, 64, 64);
+        drawTile(atlas.getWallTile(), 1, 1);
+        drawTile(atlas.getWallTile(), 2, 1);
+        drawTile(atlas.getWallTile(), 3, 1);
+        drawTile(atlas.getWallTile(), 4, 1);
+
+        drawTile(atlas.getWallTile(), 1, 2);
+        drawTile(atlas.getFloorTile(), 2, 2);
+        drawTile(atlas.getFloorTile(), 3, 2);
+        drawTile(atlas.getFloorTile(), 4, 2);
+
+        drawTile(atlas.getWallTile(), 1, 3);
+        drawTile(atlas.getWallTile(), 2, 3);
+        drawTile(atlas.getWallTile(), 3, 3);
+        drawTile(atlas.getWallTile(), 4, 3);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
+    }
+
+    private void drawTile(TextureRegion region, int tileX, int tileY) {
+        batch.draw(
+            region,
+            MAP_OFFSET_X + tileX * RENDER_TILE_SIZE,
+            MAP_OFFSET_Y + tileY * RENDER_TILE_SIZE,
+            RENDER_TILE_SIZE,
+            RENDER_TILE_SIZE
+        );
     }
 }
--- game/core/src/main/java/com/marton/roguelike/render/TileAtlas.java
+++ game/core/src/main/java/com/marton/roguelike/render/TileAtlas.java
@@ -10,21 +10,21 @@ public class TileAtlas {
     private final int TILE_WIDTH = 16;
     private final int TILE_HEIGHT = 16;
 
-    private TextureRegion floorTile;
+    private final TextureRegion floorTile;
+    private final TextureRegion wallTile;
+    private final TextureRegion treeTile;
 
 
 
     public TileAtlas() {
         this.tileSheet = new Texture("textures/tiles.png");
         floorTile = getSpecificTile(4, 0);
+        wallTile = getSpecificTile(10, 17);
+        treeTile = getSpecificTile(0,1);
     }
 
     public Texture getTileSheet() {
         return tileSheet;
-    }
-
-    public TextureRegion getFloorTile() {
-        return floorTile;
     }
 
     public TextureRegion getSpecificTile(int column, int row) {
@@ -36,4 +36,18 @@ public class TileAtlas {
             TILE_HEIGHT
         );
     }
+
+    public TextureRegion getFloorTile() {
+        return floorTile;
+    }
+
+    public TextureRegion getWallTile() {
+        return wallTile;
+    }
+
+    public TextureRegion getTreeTile() {
+        return treeTile;
+    }
+
+
 }
